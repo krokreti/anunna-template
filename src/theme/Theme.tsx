9ad58a26f1e9f926e0663be333e46391bf2c0e58
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-05-11 19:56:52 -0300

    create anunna project

---
+++ src/theme/Theme.tsx
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material/styles';
+
+export const mainTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#5A33DB',
+        },
+        text: {
+            primary: '#F2F2F7',
+            secondary: '#8E8E93',
+        },
+        secondary: {
+            main: '#8E8E93',
+        },
+        background: {
+            default: '#161616',
+        },
+    },
+});
