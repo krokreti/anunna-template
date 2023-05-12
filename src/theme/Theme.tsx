6bc2526e286571b66c0991f55a8f2a7941ab0f1c
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-05-12 14:11:56 -0300

    add fonts to project

--- src/theme/Theme.tsx
+++ src/theme/Theme.tsx
@@ -1,4 +1,5 @@
 import { createTheme } from '@mui/material/styles';
+import Friends from '../../public/Friends-Regular.ttf';
 
 export const mainTheme = createTheme({
     palette: {
@@ -16,4 +17,22 @@ export const mainTheme = createTheme({
             default: '#161616',
         },
     },
+    typography: {
+        fontFamily: 'Friends'
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: `
+            @font-face {
+                font-family: 'Friends';
+                font-style: normal;
+                font-display: swap;
+                font-weight: 400;
+                src: local('Friends'), local('Friends-Regular'), url(${Friends}) format('truetype');
+                unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+              }
+            `
+        }
+    }
+
 });
