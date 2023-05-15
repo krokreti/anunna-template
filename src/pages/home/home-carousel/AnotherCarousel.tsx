6db76afb23cc012786355aad299c0ef5cb8ab53e
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-05-15 18:00:01 -0300

    delete homecarousel

--- src/pages/home/home-carousel/AnotherCarousel.tsx
+++ src/pages/home/home-carousel/AnotherCarousel.tsx
@@ -4,7 +4,11 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 const AnotherCarousel = () => {
-
+    // "https://d8cg12l7cuw6g.cloudfront.net/wp-content/uploads/2019/08/08172459/Wallpaper-vulca%CC%83o-para-celular-de-viajantes-travel-wanderlust-iphone-paisagem.jpeg",
+    // "https://images.pexels.com/photos/799443/pexels-photo-799443.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    // "https://i.pinimg.com/originals/2f/31/4c/2f314ca9f056e2dda005cd7989622e23.jpg",
+    // "https://w.forfun.com/fetch/62/62e3ce60fc426fe6f475764cd99779b9.jpeg",
+    // "https://i.pinimg.com/736x/db/cc/65/dbcc656891295a71e99317c18e69a487--phone-wallpapers-smartphone.jpg"
     return (
         <Box display={'flex'} height={'100%'} alignItems={'end'}>
             <Swiper
