0e89a213230549d23f4f9796a385b665242d267d
davi <davipinheiro1993@gmail.com>
2023-05-16 13:10:07 -0300

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material'
 import { mainTheme } from './theme/Theme.tsx'
 import { Box } from '@mui/material';
 import Navbar from './components/layout/Navbar.tsx';
-import Home from './pages/home/Home.tsx';
+import Home from './pages/home/home.tsx';
 
 function App() {
 
