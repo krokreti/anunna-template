441136995e412c5a138e652bba8fbf4a89351ff3
davi <davipinheiro1993@gmail.com>
2023-05-18 11:30:45 -0300

    commented out

--- src/pages/home/home-carousel/AnotherCarousel.tsx
+++ src/pages/home/home-carousel/AnotherCarousel.tsx
@@ -1,5 +1,6 @@
 import { Box, useMediaQuery } from "@mui/material";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// import { useSwiper } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
