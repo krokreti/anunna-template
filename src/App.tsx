2377e3c3e6988eb8fb9cec4690ddf51175f72df3
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-05-18 18:47:01 -0300

    add navbar box background

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,24 @@ import mainTheme from './theme/Theme';
 import { Box } from '@mui/material';
 import Navbar from './components/layout/Navbar';
 import Home from './pages/home/home';
+import { useAppSelector } from './hooks/redux-hooks';
+import { currentProjectStatus } from './store/project-slice';
 
 
 function App() {
-
+  const background = useAppSelector(currentProjectStatus);
   return (
     <ThemeProvider theme={mainTheme}>
       <CssBaseline />
-      <Box>
+      <Box
+        sx={{
+          backgroundImage: `url(${background.background})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          width: '100vw',
+        }}
+      >
         <Navbar />
         <Home />
       </Box>
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,18 +5,9 @@ import { useAppSelector } from "../../hooks/redux-hooks";
 import { currentProjectStatus } from "../../store/project-slice";
 
 const Home = () => {
-    const background = useAppSelector(currentProjectStatus);
-    console.log(background)
     return (
         <Box
             height={'calc(100vh - 4em)'}
-            sx={{
-                backgroundImage: `url(${background.background})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-                width: '100vw',
-            }}
         >
             {/* <button style={{ background: 'white' }}>TESTE</button> */}
             <Grid container height={'100%'}>
