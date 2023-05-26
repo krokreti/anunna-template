40ae2f6b52b93134000777f00374080a2a02d9d2
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-05-26 19:42:00 -0300

    Change route name

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import ProjectDetails from "../pages/project-details/ProjectDetails";
-import HomePage from "../pages/home/Home";
+import HomePage from "../pages/home/HomePage";
 
 export const router = createBrowserRouter([
     {
