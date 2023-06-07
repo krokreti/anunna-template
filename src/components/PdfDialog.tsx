7a433b10434584b968dec568a23bd27c47a72b93
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-06-07 01:39:13 -0300

    finish dialog

--- src/components/PdfDialog.tsx
+++ src/components/PdfDialog.tsx
@@ -1,28 +1,35 @@
-import { Dialog, DialogActions, DialogContent } from "@mui/material";
-import CustomButton from "./CustomButton";
+import { Dialog, DialogContent, Fade } from "@mui/material";
+import React from "react";
 
 const PdfDialog: React.FC<{ open: boolean, close: () => void, pdfUrl?: string, id: string }> = (props) => {
-
+    const iframeStyle: React.CSSProperties = {
+        height: '92.7vh',
+    };
     return (
         <Dialog
             open={props.open}
             PaperProps={{ style: { backgroundColor: '#161616', } }}
+            onClose={props.close}
+            TransitionComponent={Fade}
+            transitionDuration={{ enter: 1000, exit: 1000 }}
+            maxWidth={"lg"}
             fullWidth
-            fullScreen
+            scroll="body"
         >
-            <DialogContent>
+            <DialogContent >
                 <iframe src={`/pdf/${props.id}.pdf#toolbar=0&navpanes=0`}
                     width="100%"
-                    height="95%"
+                    height="100%"
                     scrolling="no"
                     allowFullScreen
                     frameBorder={0}
+                    style={iframeStyle}
                 >
                 </iframe>
             </DialogContent>
-            <DialogActions>
+            {/* <DialogActions>
                 <CustomButton onClick={props.close} variant='text'>Close</CustomButton>
-            </DialogActions>
+            </DialogActions> */}
         </Dialog>
     )
 }
